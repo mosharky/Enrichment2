3323da55bb8112e921132b2a4301e555e21d1db0
Qry <qryseymour@gmail.com>
2025-01-13 23:08:38 -0500

    Modified some vanilla recipes

--- kubejs/server_scripts/replace.js
+++ kubejs/server_scripts/replace.js
@@ -3,6 +3,23 @@ function replacements(e) {
     e.replaceInput({}, 'spelunkery:rock_salt', 'galosphere:pink_salt_shard')
     e.replaceInput({}, 'galosphere:silver_block', '#c:silver_blocks')
     e.replaceInput({}, 'friendsandfoes:crab_claw', 'ecologics:crab_claw')
+    e.replaceInput({}, 'minecraft:quartz', '#c:gems/quartz')
+    
+    let ironToCopperRecipeItems = [
+        'minecraft:piston',
+        'minecraft:compass',
+        'minecraft:powered_rail'
+    ].forEach((item) => {
+        e.replaceInput({ output: item }, 'minecraft:iron_ingot', 'minecraft:copper_ingot')
+    })
+
+    let glassToAmethystRecipeItems = [
+        'minecraft:beacon',
+        'minecraft:end_crystal'
+    ].forEach((item) => {
+        e.replaceInput({ output: item }, 'minecraft:glass', 'minecraft:amethyst_shard')
+    })
+    
 
     // fixing ice and fire inputs
     // e.replaceInput({}, '#c:storage_blocks/silver', '#c:silver_blocks')
